0ddf22e865bd034a65c1d0cd392e429650890ce8
George <mahobintechnology@gmail.com>
2026-01-09 12:55:39 +0600

    Refactor Appwrite client initialization

--- js/appwrite.js
+++ js/appwrite.js
@@ -2,15 +2,15 @@ import {
   Client,
   Account,
   Databases,
-  Storage
+  Storage,
+  ID
 } from "https://cdn.jsdelivr.net/npm/appwrite@13.0.1/dist/esm/sdk.js";
 
-const client = new Client();
-
-client
-  .setEndpoint("https://cloud.appwrite.io/v1") // or self-hosted endpoint
-  .setProject("69609e88000a6d84dde4");
+const client = new Client()
+  .setEndpoint("https://cloud.appwrite.io/v1") // or your self-hosted endpoint
+  .setProject("YOUR_PROJECT_ID");
 
 export const account = new Account(client);
 export const databases = new Databases(client);
 export const storage = new Storage(client);
+export { ID };
